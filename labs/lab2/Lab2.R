e5e506561fb4b22d192fdf2dbddcd628eb8becbc
Ayobamidele <paulikeystreble@gmail.com>
2023-10-18 04:18:53 +0100

    Assignment 2 Done

--- labs/lab2/Lab2.R
+++ labs/lab2/Lab2.R
@@ -2,7 +2,7 @@
 ###################################
 ########                   ########
 ########   Data Science 2  ########
-########       Lab 2       ######## 
+########       Lab 2       ########
 ########  7th Feb. 2023    ########
 ########                   ########
 ###################################
@@ -23,7 +23,7 @@ matrix(1:9, byrow = T, nrow = 3)
 
 # Now is a good time to point out that R will automatically read T as TRUE and F as FALSE, which can be a time-saver.
 
-# Let's define 3 vectors. Each one represents the box office numbers from the first three Star Wars movies. 
+# Let's define 3 vectors. Each one represents the box office numbers from the first three Star Wars movies.
 # The first element of each vector indicates the US box office revenue,
 # the second element refers to the Non-US box office.
 
@@ -35,6 +35,7 @@ return_jedi <- c(309.306, 165.8)
 # Create box_office vector
 box_office <- c(new_hope, empire_strikes, return_jedi)
 
+box_office
 # Construct star_wars_matrix
 star_wars_matrix <- matrix(box_office, byrow = T, nrow = 3)
 star_wars_matrix
@@ -43,43 +44,72 @@ star_wars_matrix
 
 rownames(star_wars_matrix) <- c("A new hope", "The empire strikes back", "Return of the Jedi")
 colnames(star_wars_matrix) <- c("US revenue", "International revenue")
-
+star_wars_matrix
 ### 1.1
 # Some important base R functions to know: colSums, rowSums, colMeans, rowMeans
 # Use one of the above functions to calculate the total revenue for each movie (the sum of the US and international revenue)
 # and save it in an object called total_revenue
 
+total_revenue <- rowSums(star_wars_matrix)
+total_revenue
 # We can now add this vector as a new column using the function cbind (column bind)
 
 star_wars_matrix <- cbind(star_wars_matrix, total_revenue)
+star_wars_matrix
 
 ### 1.2
 # Rename the 3rd element of the column names of star_wars_matrix to "Total revenue"
+colnames(star_wars_matrix)[3]
+colnames(star_wars_matrix)[3] <- "Total revenue"
 
+star_wars_matrix
 # Now lets create vectors for the box office returns of the prequel trilogy
 
-phantom_menace <- c(474.5,552.5)
-attack_of_clones <- c(310.7,338.7)
+phantom_menace <- c(474.5, 552.5)
+attack_of_clones <- c(310.7, 338.7)
 revenge_of_sith <- c(380.3, 468.5)
 
+prequel_trilogy <- c(phantom_menace, attack_of_clones, revenge_of_sith)
+
+prequel_trilogy
+
 ### 1.3
-# Turn these 3 vectors into a matrix, add a column for total revenue, 
+# Turn these 3 vectors into a matrix, add a column for total revenue,
+
+star_wars_matrix <- matrix(prequel_trilogy, byrow = T, nrow = 3)
+star_wars_matrix
+
+rownames(star_wars_matrix) <- c("phantom_menace", "attack_of_clones", "revenge_of_sith")
+colnames(star_wars_matrix) <- c("US revenue", "International Revenue")
+star_wars_matrix
+
+Total_revenue <- rowSums(star_wars_matrix)
+
+
+Total_revenue
 # and append them to star_wars_matrix using the function rbind (row bind)
 
+star_wars_matrix <- cbind(star_wars_matrix, Total_revenue)
+
+star_wars_matrix
+
 # Matrices are understood by R to be both one-dimensional, because they are vectors folded onto themselves
-# into columns, but also 2 dimensional, because they have rows and columns. 
+# into columns, but also 2 dimensional, because they have rows and columns.
 # So you can index them like this:
+
+star_wars_matrix
+
 star_wars_matrix[4]
 
 # but also like this
-star_wars_matrix[3,1]
+star_wars_matrix[3, 1]
 
 # When you put a comma in between square brackets, you are indexing both the rows (to the left of the comma),
 # and the columns (to the right of the comma).
 
-### 1.4 
+### 1.4
 # Write a line of code to extract the international revenue of the Phantom Menace (the 4th movie) using numbers to index the matrix
-
+star_wars_matrix[1, 2]
 # Compare this to the following:
 star_wars_matrix["The Phantom Menace", "Total revenue"]
 
@@ -87,17 +117,17 @@ star_wars_matrix["The Phantom Menace", "Total revenue"]
 
 # If you try to include a comma in the index of a one-dimensional object (like a vector),
 # you get an error message:
-new_hope[1,2]
+new_hope[1, 2]
 
 # If you leave a blank space either to the left or the right of the comma, you select for ALL the rows
 # or ALL the columns, respectively
-star_wars_matrix[,2]
-star_wars_matrix[4,]
+star_wars_matrix[, 2]
+star_wars_matrix[4, ]
 
 # Just like multiplying 2 vectors of the same length together will multiply each of the corresponding elements
 # the same will work with 2 matrices of the same dimensions
 
-star_wars_matrix[1:3,] * star_wars_matrix[4:6,] 
+star_wars_matrix[1:3, ] * star_wars_matrix[4:6, ]
 
 # But for matrix multiplication (the kind used in linear algebra), you must use %*%
 
@@ -105,32 +135,34 @@ star_wars_matrix[1:3,] * star_wars_matrix[4:6,]
 ####    Lists    ####
 #####################
 
-# Lists are extremely useful objects in R. 
+# Lists are extremely useful objects in R.
 # With a list, you can save lots of different objects into one object.
 
-my_list <- list(c("red,green"), 1:5, FALSE, matrix(1:9,3,3))
+my_list <- list(c("red,green"), 1:5, FALSE, matrix(1:9, 3, 3))
 my_list
 
 my_list[3]
 
 # You can even have lists inside of lists, recursively
-my_list_2 <- list(my_list,c("Hello","World"))
+my_list_2 <- list(my_list, c("Hello", "World"))
 my_list_2
 
 # You cane name the elements of a list with names()
-names(my_list) <- c("colours","digits","boolean","matrix")
+names(my_list) <- c("colours", "digits", "boolean", "matrix")
 my_list
 
 # and you can access each element of a list with the $ operator
 my_list$boolean
 
 ### 2.1
-# Using the $ operator, replace the "matrix" element of my_list with the star_wars_matrix
+# Using the $ operator, replace the "matrix" element of my_list with the star_wars_matrix # nolint
+my_list$matrix <- "star_wars_matrix"
+my_list
 
 # Finally, you can turn any list into a vector with unlist().
 unlist(my_list)
 
-# If the classes of each of the elements are different, it will automatically convert them all 
+# If the classes of each of the elements are different, it will automatically convert them all
 # to the 'character' class so they are all compatible
 
 ###########################
@@ -139,11 +171,11 @@ unlist(my_list)
 
 # Data frames are also 2-dimensional objects, with rows and columns.
 # However, data frames are not, like matrices, single vectors folded onto themselves.
-# Instead, they are special kinds of list, where each element of the list is a vector 
+# Instead, they are special kinds of list, where each element of the list is a vector
 # of the same length, but possibly of different classes, each of which is a column of the
 # data frame.
 
-# Most data you will work with in R will be in a data frame. 
+# Most data you will work with in R will be in a data frame.
 # Lets load one that comes automatically with your R installation:
 
 data("iris")
@@ -167,7 +199,7 @@ iris$Species
 hist(iris$Sepal.Length)
 
 # Next, a scatterplot
-plot(iris$Petal.Length,iris$Petal.Width)
+plot(iris$Petal.Length, iris$Petal.Width)
 
 
 ###############################
@@ -176,9 +208,9 @@ plot(iris$Petal.Length,iris$Petal.Width)
 
 ## SETTING THE SEED
 # Remember this?
-sample(1:100,1)
-sample(1:100,1)
-sample(1:100,1)
+sample(1:100, 1)
+sample(1:100, 1)
+sample(1:100, 1)
 
 # Those of you with background in computer science or data science probably know
 # that true randomness doesn't really exist in comupters. Instead, the appearance of randomness
@@ -188,17 +220,17 @@ sample(1:100,1)
 # As a result, we can set our own seed and generate our own randomness:
 
 set.seed(123)
-sample(1:100,1)
+sample(1:100, 1)
 
 # You got 31, right? Run it again and see:
 
 set.seed(123)
-sample(1:100,1)
+sample(1:100, 1)
 
 # Any time we want to make sure we get the same results as one another despite having some
 # element of random sampling in out code, we will set the seed to be the same.
 
-## RANDOM DISTRIBUTIONS 
+## RANDOM DISTRIBUTIONS
 
 # Remember the formula for the Binomial distribution:
 # 𝑷(W=k) =  𝒑^𝒌∗(𝟏−𝒑)^((𝑵−𝒌) )∗(𝒏¦𝒌)
@@ -211,12 +243,29 @@ sample(1:100,1)
 # To do this you will need to use either the factorial() function
 # or the choose() function
 
+?choose()
+
+calculate_probability <- function(n, k, p) {
+  binomial_coefficient <- choose(n, k)
+
+  probability <- binomial_coefficient * p^k * (1 - p)^(n - k)
+
+  return(probability)
+}
 # use your function to calculate the probability that when the aliens send 10 probes to Earth (probability of water = 0.7),
 # exactly 8 of those probes will send a signal of water
+get_probability <- calculate_probability(10, 8, 0.7)
 
+get_probability
 # compare this to dbinom(8,10,0.7)
 
-### PROBABILITY FUNCTIONS IN R 
+dbinom(8, 10, 0.7)
+
+# My function calculate_probability() and dbinom () both give the probability of success
+# to be 0.2334744. This means that dbinom() is a built-in function
+# that calculates the PMF of a binomial distributions
+
+### PROBABILITY FUNCTIONS IN R
 # dbinom, dnorm, dunif, dbeta, .... all of these functions calculate f(x) for any given x
 # for each of their relative distributions
 # in other words, they calculate probability MASS or probability DENSITY for each of
@@ -243,59 +292,54 @@ sample(1:100,1)
 
 # So for example, if a person of 195 cm was drawn from a gaussian distribution of heights,
 # with mean 175cm and standard deviation 10cm, What percentile would they be in?
-pnorm(195, 175, 10, lower.tail=FALSE)
+pnorm(195, 175, 10, lower.tail = FALSE)
 # They would be in the upper 2.3 percentile
 
 # qbinom, qnorm, qunif, qbeta, .... all calculate value of x for which
-# the area under the curve of a given distribution in the lower tail 
+# the area under the curve of a given distribution in the lower tail
 # is equal to the given number
 
 # For example, in a Gaussian distribution of people's heights with mean 175cm and standard deviation 10cm,
-# How tall is someone in the bottom 10th percentile? 
+# How tall is someone in the bottom 10th percentile?
 qnorm(0.1, 175, 10)
 # In the upper 10th percentile?
 qnorm(0.9, 175, 10)
 # Compare this to:
-qnorm(0.1,175, 10, lower.tail=FALSE)
+qnorm(0.1, 175, 10, lower.tail = FALSE)
 
 ### 4.3
 # Let's compare the box office returns of the og and prequel trilogies
-og_trilogy <- star_wars_matrix[1:3,3]
-preq_trilogy <- star_wars_matrix[4:6,3]
+og_trilogy <- star_wars_matrix[1:3, 3]
+preq_trilogy <- star_wars_matrix[4:6, 3]
 
 # Write a Welch's t-test function for any two samples x1 and x2
-my_t <- function(x1,x2){
+my_t <- function(x1, x2) {
   # first, extract the means, variances and Ns of the two samples and save thel to
-  n1 <- 
-  m1 <-
-  s1 <- 
-  n2 <-
-  m2 <- 
-  s2 <- 
- 
-  # next, calculate the average standard deviation using the formula shown in the class on slide 44:
- 
-  s <- 
+  n1 <-
+    m1 <-
+    s1 <-
+    n2 <-
+    m2 <-
+    s2 <-
+    # next, calculate the average standard deviation using the formula shown in the class on slide 44:
 
-  # next, calculate the t-statistic, again as shown on slide 44
- 
-  t <- 
- 
- 
-  # next, calculate the degrees of freedom (again see slide 44)
-  # make sure you use parentheses correctly here
- 
-  df <- 
- 
-  # next, calculate the probability that the t-statistic would be greater than the absolute value of the t-statistic that you calculated if the TRUE difference between the groups was 0
-  # to do this, you can use function pt
-  p_value <- pt(abs(t), df=df, lower.tail = F)*2
+    s <-
+    # next, calculate the t-statistic, again as shown on slide 44
 
-  return(list(t = t, df = df, p_value=p_value))
-  }
+    t <-
+    # next, calculate the degrees of freedom (again see slide 44)
+    # make sure you use parentheses correctly here
+
+    df <-
+    # next, calculate the probability that the t-statistic would be greater than the absolute value of the t-statistic that you calculated if the TRUE difference between the groups was 0
+    # to do this, you can use function pt
+    p_value <- pt(abs(t), df = df, lower.tail = F) * 2
+
+  return(list(t = t, df = df, p_value = p_value))
+}
 
 # compare this function to the in-built t-test
-t.test(og_trilogy,preq_trilogy)
-my_t(og_trilogy,preq_trilogy)
+t.test(og_trilogy, preq_trilogy)
+my_t(og_trilogy, preq_trilogy)
 
 # One last question to ponder before next class: Why did we multiply the p-value by 2?
