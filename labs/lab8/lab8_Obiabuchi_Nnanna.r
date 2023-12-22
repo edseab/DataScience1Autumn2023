7c98e02734f4e010a7a8d11a710de975d994f191
Nnanna, Obiabuchi Martin <martinobiabuchi@yahoo.com>
2023-12-22 05:14:03 +0100

    working now

--- labs/lab8/lab8_Obiabuchi_Nnanna.r
+++ labs/lab8/lab8_Obiabuchi_Nnanna.r
@@ -209,22 +209,52 @@ Unique_Car_type<- unique(toycars$car)
 Unique_Car_type
 d<-toycars[toycars$car==1,]
 d
-
 sum(toycars$car)
 sum(toycars[toycars$car==1,]$car)
 sum(toycars[toycars$car==2,]$car)
 sum(toycars[toycars$car==3,]$car)
 
+#To check whether the dependent variable follows a normal distribution, use the hist() function.
+hist(toycars$distance) #the distance variable almost shows  normal distrubutin
+plot(toycars$distance,toycars$car) 
+?cor()
+cor(toycars$distance,toycars$car)
 # We want to model the relationship between the type of car and the distance they travel. 
+model_car_dist <- lm(toycars$car~toycars$distance, data=toycars)
+model_car_dist
+summary(model_car_dist)
+
+?lm()
 # 3.2 Which variable should be the outcome (independent) variable? Why?
 
+#The outcome variable is the car type because I want to explain the distance travelled by the type of car.
+
+
 # 3.3 Run a regression model with the formula distance ~ car. How would you interpret the regression coefficient? What is the problem with this model? How would you rectify this problem?
-9
+
+model_dist_car<- lm(toycars$distance~toycars$car, data=toycars)
+model_dist_car
+summary(model_dist_car)
+
 # 3.4 Recode the 'car' variable so that car 1 is coded as 'green', car 2 is 'yellow', and car 3 is 'red'
 
-# 3.5 Rerun the regression model using this recoded variable and interpret the coefficients. What does this model say about whether how choosing different cars affects the distances they travel?
+df_toycars<- data.frame(toycars)
 
+df_toycars$car <- factor(df_toycars$car, levels = c(1, 2, 3), labels = c('green', 'yellow', 'red'))
+df_toycars$car
+df_toycars
+
+# 3.5 Rerun the regression model using this recoded variable and interpret the coefficients. What does this model say about whether how choosing different cars affects the distances they travel?
+df_toycars$car
+model2<-lm(df_toycars$distance~df_toycars$car,data=df_toycars)
+model2
+summary(model2)
+?factor()
 # 3.6 Rerun the model again, this time including 'angle' as an independent variable. Interpret all 4 coefficients.
+
+model3<-lm(distance~car+angle,data=df_toycars)
+model3
+summary(model3)
 
 # 3.7 What is the predicted distance traveled by a red car launched at 3 degrees?
 
