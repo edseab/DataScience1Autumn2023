0f7522d44edfb0b78a52ee9529515c5b743de14f
Ed Seabright <ed.seabright@gmail.com>
2024-01-09 15:58:08 +0100

    lab 8 answers

--- labs/lab8/lab 8_answers.r
+++ labs/lab8/lab 8_answers.r
@@ -181,11 +181,19 @@ mod_rescyl_resmpg <- lm(mpg_wt_residuals~cyl_wt_residuals)
 
 # 2.7 Based on your answers to 2.3 and 2.5, how do you interpret the slope estimate of the previous regression?
 
-# 2.8 Compare this estimate to the estimate of wt in the original full model. 
+# It is the predicted change in fuel efficiency associated with having one additional cylinder, AFTER the effects of weight have been accounted for.
+
+# 2.8 Compare this estimate to the estimate of cyl in the original full model. 
 # What do you conclude?
 
-# 2.9 Use the same method to get the estimate of cyl from the original model using only simple linear regressions.
+# The estimates are identical. This means that in the multiple regression the estimate of cyl is adjusted to account for the 
+# effect of wt, and vice versa
 
+# 2.9 Use the same method to get the estimate of wt from the original model using only simple linear regressions.
+
+mpg_cyl_residuals <- residuals(lm(mpg ~ cyl, data=mtcars))
+wt_cyl_residuals <- residuals(lm(wt ~ cyl, data=mtcars))
+mod_reswt_resmpg <- lm(mpg_cyl_residuals~wt_cyl_residuals)
 
 
 #############################
@@ -206,24 +214,74 @@ data(toycars)
 # There are 3 different types of toy cars, numbered 1, 2, and 3
 
 # We want to model the relationship between the type of car and the distance they travel. 
-# 3.2 Which variable should be the outcome (independent) variable? Why?
+# 3.2 Which variable should be the outcome (dependent) variable? Why?
+
+# Car type can affect distance traveled, but not the reverse. Therefore, distance traveled should be the outcome variable
 
 # 3.3 Run a regression model with the formula distance ~ car. How would you interpret the regression coefficient? What is the problem with this model? How would you rectify this problem?
 
+m1 <- lm(distance ~ car, data=toycars)
+
+# The 'car' coefficient represents the predicted change in distance for a 1 'unit' change in car. 
+# The problem is that the numbers for the 'car' variable don't represent a numeric value and don't have a unit
+# Instead they represent a categorical variable, the type of car
+# So the estimate is the predicted difference between car 1 and car 2, and between car 2 and car 3, and half the difference between
+# car 1 and car 3 - even though these might not be well estimated using the same number
+# Instead they should be modeled using a categorical variable.
+
 # 3.4 Recode the 'car' variable so that car 1 is coded as 'green', car 2 is 'yellow', and car 3 is 'red'
 
-# 3.5 Rerun the regression model using this recoded variable and interpret the coefficients. What does this model say about whether how choosing different cars affects the distances they travel?
+library(dplyr)
+
+toycars |> mutate(car_colour = case_when ( car == 1 ~ 'green',
+                                    car == 2 ~ 'yellow',
+									car == 3 ~ 'red')) -> toycars
+									
+
+
+# 3.5 Rerun the regression model using this recoded variable and interpret the coefficients. 
+# What does this model say about whether how choosing different cars affects the distances they travel?
+
+m2 <- lm(distance ~ car_colour, data=toycars)
+summary (m2)
+
+# The estimates for red cars and yellow cars - relative to green cars - are not significantly larger than 0. 
+# This indicates that - according to this model - there is no association between car type and distances traveled.
 
 # 3.6 Rerun the model again, this time including 'angle' as an independent variable. Interpret all 4 coefficients.
 
+m3 <- lm(distance ~ car_colour + angle, data=toycars)
+summary (m3)
+
+# The intercept is the predicted distance traveled for the reference category car (green) launched at 0 degrees
+# The car_colourRed and car_colourYellow estimatesare the predicted differences between the distance
+# traveled by the reference category car (green) and the red and yellow respectivelholding the angle launched constant
+
+# The angle estimate is the predicted change in distance launched for each additional degree of slope that the car was launched at,
+# holding the type of car constant
+
+
 # 3.7 What is the predicted distance traveled by a red car launched at 3 degrees?
 
-# 3.8 What is the predicted distance traveled by a white car launched at 2.5 degrees?
+0.092524 + (-0.082222) + 3*0.188541
+# The predicted distance is 0.575925 metres
+
+# 3.8 What is the predicted distance traveled by a green car launched at 2.5 degrees?
+0.092524 + 2.5*0.188541
+# The predicted distance is 0.575925 metres
+
 
 # 3.9 Plot the results of this model using ggplot with 90% confidence intervals and appropriate colors
-
-
-
-
-
+toycars <- cbind(toycars,predict(m3, interval='confidence'))
+library(ggplot2)
+png(filename='toycars_model.png',width=800, height= 641)
+ggplot(toycars,aes(x=angle,y=distance,colour=car_colour))+
+	geom_point()+
+	geom_line(aes(y=fit))+
+	geom_ribbon(aes(ymax=upr,ymin=lwr, fill=car_colour),alpha=0.2)+
+	theme_minimal()+
+	scale_color_manual(values= c("green", 'yellow','red'))+
+    scale_fill_manual(values= c("green", 'yellow','red'))+
+	labs(title='Results of experiment launching toy cars at various angles', x='Angle of launch (degrees)', y='Distance traveled (metres)', fill='Car type')
+dev.off()
 
